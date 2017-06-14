5acfca957e1cbb13b91296598270e7d7ebd4068c
Jon Courtney <jon.courtney@gmail.com>
2017-06-14 10:09:21 -0600

    Added argparse, updated inheritance, added test for number of classes

--- annotate/annotate.py
+++ annotate/annotate.py
@@ -1,19 +1,20 @@
 # annotate.py
 #
-# import pandas as pd
-# import rosbag_pandas
-# from PIL import Image, ImageDraw
+import pandas as pd
+import rosbag_pandas
+from PIL import Image, ImageDraw
 from io import BytesIO
-#import numpy as np
+import numpy as np
 import argparse, sys, pdb
 
-#from ImageWindow import ImageWindow
+from imagewindow import ImageWindow
 from shared.action import Action
 from base import AnnotateBase
 
 class Annotator(AnnotateBase):
     def __init__(self):
-        super().__init__()
+        super(Annotator, self).__init__()
+
 
     def _load_bag_data(self, file):
         bag = rosbag_pandas.bag_to_dataframe(file)
@@ -26,12 +27,14 @@ class Annotator(AnnotateBase):
 
         assert self.width==856 and self.height==480, "Unexpected image dimensions (%d, %d)" % (self.width, self.height)
 
+
     def annotate(self, file):
         self._load_bag_data(file)
         w = self.width
         h = self.height
         s = self.scale
-        size = w*h/s/s*3
+        c = self.chans
+        size = w*h/s/s*c
         iw = ImageWindow(w, h)
         self.labels = np.empty(self.num_images, dtype='byte')
         self.data = np.empty((self.num_images, size), dtype='byte')
@@ -81,10 +84,10 @@ class Annotator(AnnotateBase):
                 label = action.TARGET_LEFT
             elif key=='Right':
                 label = action.TARGET_RIGHT
-            # elif key=='Up':
-            #     label = action.TARGET_UP
-            # elif key=='Down':
-            #     label = action.TARGET_DOWN
+            elif self.num_actions > 4 and key=='Up':
+                label = action.TARGET_UP
+            elif self.num_actions > 4 and key=='Down':
+                label = action.TARGET_DOWN
             else:
                 label = action.SCAN
 
@@ -96,16 +99,21 @@ class Annotator(AnnotateBase):
         iw.close()
         self.num_annotated = i
 
+
     def save(self, outfile):
         n = self.num_annotated
         np.savez(outfile, data=self.data[:n], labels=self.labels[:n])
+
 
     def convert(self, bagfile, npzfile_in, npzfile_out, mode):
         self._load_data(bagfile)
         w = self.width
         h = self.height
         s = self.scale
-        size = w*h/s/s*3
+        c = self.chans
+        size = w*h/s/s*c
+
+        assert mode in ['RGB', 'HSV', 'YCrCb']
 
         npz = np.load(npzfile_in)
         labels = self.labels = npz['labels']
@@ -117,41 +125,24 @@ class Annotator(AnnotateBase):
 
         for i in range(n):
             image = Image.open(BytesIO(self.image_data[i]))
+            assert image.mode == 'RGB'
             resized = image.resize((w/s, h/s), resample=Image.LANCZOS)
-            hsv = resized.convert(mode)
-            data[i] = np.fromstring(hsv.tobytes(), dtype='byte')
+            if mode=='RGB':
+                new_img = resized
+            else:
+                new_img = resized.convert(mode)
+            data[i] = np.fromstring(new_img.tobytes(), dtype='byte')
             print "%d : %d" % (i, n)
 
         np.savez(npzfile_out, data=data, labels=labels)
 
-    def convert_rgb(self, bagfile, npzfile_in, npzfile_out):
-        self._load_data(bagfile)
-        w = self.width
-        h = self.height
-        s = self.scale
-        size = w*h/s/s*3
-
-        npz = np.load(npzfile_in)
-        labels = self.labels = npz['labels']
-        n = self.num_annotated = labels.shape[0]
-
-        assert self.num_images == self.num_annotated
-
-        data = self.data = np.empty((self.num_images, size), dtype='byte')
-
-        for i in range(n):
-            image = Image.open(BytesIO(self.image_data[i]))
-            rgb = image.resize((w/s, h/s), resample=Image.LANCZOS)
-            assert rgb.mode == 'RGB'
-            data[i] = np.fromstring(rgb.tobytes(), dtype='byte')
-            print "%d : %d" % (i, n)
-
-        np.savez(npzfile_out, data=data, labels=labels)
 
 def get_args():
     parser = argparse.ArgumentParser(description='Annotate drone flight images with action commands for supervised learning. NOTE: Python 2 required.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('infile', metavar='in_bagfile', help='bagfile to analyze')
-    parser.add_argument('outfile', metavar='out_npzfile', help='npz file for saving results')
+    parser.add_argument('infile', metavar='bagfile_in', help='bagfile to analyze')
+    parser.add_argument('outfile', metavar='npzfile_out', help='npz file for writing results')
+    parser.add_argument('--convert', metavar='npzfile_in', help='npz file to convert')
+    parser.add_argument('--mode', default='RGB', choices=['RGB', 'HSV', 'YCrCb'], help='Image format to which to convert')
 
     return parser.parse_args()
 
@@ -160,31 +151,9 @@ if __name__ == '__main__':
     args = get_args()
 
     a = Annotator()
-    a.annotate(args.infile)
-    a.save(args.outfile)
 
-    # if sys.argv[1] == '--play':
-    #     a = Annotator()
-    #     a.play(sys.argv[2])
-    # elif sys.argv[1] == '--hsv':
-    #     if len(sys.argv) < 5:
-    #         print("Usage: python annotate.py --hsv <bagfile> <npzfile_in> <npzfile_out>")
-    #         sys.exit()
-    #     a = Annotator()
-    #     a.convert(sys.argv[2], sys.argv[3], sys.argv[4], 'HSV')
-    # elif sys.argv[1] == '--ycbcr':
-    #     if len(sys.argv) < 5:
-    #         print("Usage: python annotate.py --hsv <bagfile> <npzfile_in> <npzfile_out>")
-    #         sys.exit()
-    #     a = Annotator()
-    #     a.convert(sys.argv[2], sys.argv[3], sys.argv[4], 'YCbCr')
-    # elif sys.argv[1] == '--rgb':
-    #     if len(sys.argv) < 5:
-    #         print("Usage: python annotate.py --rgb <bagfile> <npzfile_in> <npzfile_out>")
-    #         sys.exit()
-    #     a = Annotator()
-    #     a.convert_rgb(sys.argv[2], sys.argv[3], sys.argv[4])
-    # else:
-    #     a = Annotator()
-    #     a.annotate(sys.argv[1])
-    #     a.save(sys.argv[2])
+    if args.convert:
+        a.convert(args.infile, args.outfile, args.convert, args.mode)
+    else:
+        a.annotate(args.infile)
+        a.save(args.outfile)
