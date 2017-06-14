8a50c4a66540b970c478b78bbfd51e20c4497054
Jon Courtney <jon.courtney@gmail.com>
2017-06-14 06:48:16 -0600

    Added argparse, factored out play()

--- annotate/annotate.py
+++ annotate/annotate.py
@@ -1,15 +1,14 @@
 # annotate.py
-# Usage: ...
 #
-import pandas as pd
-import rosbag_pandas
-from PIL import Image, ImageDraw
+# import pandas as pd
+# import rosbag_pandas
+# from PIL import Image, ImageDraw
 from io import BytesIO
-import numpy as np
-import sys
-from ImageWindow import ImageWindow
-from Action import Action
-import pdb
+#import numpy as np
+import argparse, sys, pdb
+
+#from ImageWindow import ImageWindow
+from shared.Action import Action
 
 
 class Annotator:
@@ -23,7 +22,7 @@ class Annotator:
         self.data = None
         self.num_annotated = 0
 
-    def _load_data(self, file):
+    def _load_bag_data(self, file):
         bag = rosbag_pandas.bag_to_dataframe(file)
         bag = bag.rename(columns={'bebop_image_raw_throttle_compressed__data': 'data', 'bebop_image_raw_throttle_compressed__format': 'format'})
 
@@ -35,7 +34,7 @@ class Annotator:
         assert self.width==856 and self.height==480, "Unexpected image dimensions (%d, %d)" % (self.width, self.height)
 
     def annotate(self, file):
-        self._load_data(file)
+        self._load_bag_data(file)
         w = self.width
         h = self.height
         s = self.scale
@@ -89,10 +88,10 @@ class Annotator:
                 label = action.TARGET_LEFT
             elif key=='Right':
                 label = action.TARGET_RIGHT
-            elif key=='Up':
-                label = action.TARGET_UP
-            elif key=='Down':
-                label = action.TARGET_DOWN
+            # elif key=='Up':
+            #     label = action.TARGET_UP
+            # elif key=='Down':
+            #     label = action.TARGET_DOWN
             else:
                 label = action.SCAN
 
@@ -103,39 +102,6 @@ class Annotator:
 
         iw.close()
         self.num_annotated = i
-
-
-    def play(self, file):
-        npz = np.load(file)
-        data = self.data = npz['data']
-        labels = self.labels = npz['labels']
-        n = self.num_annotated = data.shape[0]
-        w = self.width = 214
-        h = self.height = 120
-        size = w*h*3
-        iw = ImageWindow(w, h)
-        action = Action()
-
-        assert size == data[0].size, "Buffer sizes do not match (%d vs %d)" % (size, data[0].size)
-
-        i = 0
-        while i < n:
-            image = Image.frombytes('HSV', (w, h), data[i])
-            iw.show_image(image)
-            iw.force_focus()
-            print 'Image %d / %d: %s' % (i, n, action.name(labels[i]))
-            iw.wait()
-
-            key = iw.get_key()
-
-            if key=='Escape':
-                break
-            elif key=='BackSpace':
-                if i > 0:
-                    i -= 1
-                continue
-            else:
-                i += 1
 
     def save(self, outfile):
         n = self.num_annotated
@@ -189,33 +155,43 @@ class Annotator:
 
         np.savez(npzfile_out, data=data, labels=labels)
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: python annotate.py <bagfile> <npzfile>")
-        sys.exit()
+def get_args():
+    parser = argparse.ArgumentParser(description='Annotate drone flight images with action commands for supervised learning. NOTE: Python 2 required.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('infile', metavar='in_bagfile', help='bagfile to analyze')
+    parser.add_argument('outfile', metavar='out_npzfile', help='npz file for saving results')
 
-    if sys.argv[1] == '--play':
-        a = Annotator()
-        a.play(sys.argv[2])
-    elif sys.argv[1] == '--hsv':
-        if len(sys.argv) < 5:
-            print("Usage: python annotate.py --hsv <bagfile> <npzfile_in> <npzfile_out>")
-            sys.exit()
-        a = Annotator()
-        a.convert(sys.argv[2], sys.argv[3], sys.argv[4], 'HSV')
-    elif sys.argv[1] == '--ycbcr':
-        if len(sys.argv) < 5:
-            print("Usage: python annotate.py --hsv <bagfile> <npzfile_in> <npzfile_out>")
-            sys.exit()
-        a = Annotator()
-        a.convert(sys.argv[2], sys.argv[3], sys.argv[4], 'YCbCr')
-    elif sys.argv[1] == '--rgb':
-        if len(sys.argv) < 5:
-            print("Usage: python annotate.py --rgb <bagfile> <npzfile_in> <npzfile_out>")
-            sys.exit()
-        a = Annotator()
-        a.convert_rgb(sys.argv[2], sys.argv[3], sys.argv[4])
-    else:
-        a = Annotator()
-        a.annotate(sys.argv[1])
-        a.save(sys.argv[2])
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    a = Annotator()
+    a.annotate(args.infile)
+    a.save(args.outfile)
+
+    # if sys.argv[1] == '--play':
+    #     a = Annotator()
+    #     a.play(sys.argv[2])
+    # elif sys.argv[1] == '--hsv':
+    #     if len(sys.argv) < 5:
+    #         print("Usage: python annotate.py --hsv <bagfile> <npzfile_in> <npzfile_out>")
+    #         sys.exit()
+    #     a = Annotator()
+    #     a.convert(sys.argv[2], sys.argv[3], sys.argv[4], 'HSV')
+    # elif sys.argv[1] == '--ycbcr':
+    #     if len(sys.argv) < 5:
+    #         print("Usage: python annotate.py --hsv <bagfile> <npzfile_in> <npzfile_out>")
+    #         sys.exit()
+    #     a = Annotator()
+    #     a.convert(sys.argv[2], sys.argv[3], sys.argv[4], 'YCbCr')
+    # elif sys.argv[1] == '--rgb':
+    #     if len(sys.argv) < 5:
+    #         print("Usage: python annotate.py --rgb <bagfile> <npzfile_in> <npzfile_out>")
+    #         sys.exit()
+    #     a = Annotator()
+    #     a.convert_rgb(sys.argv[2], sys.argv[3], sys.argv[4])
+    # else:
+    #     a = Annotator()
+    #     a.annotate(sys.argv[1])
+    #     a.save(sys.argv[2])
