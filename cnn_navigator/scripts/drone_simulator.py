cb0a857272c125158228d0a2758c8e733947dc92
Jon Courtney <jon.courtney@gmail.com>
2017-06-16 16:02:10 -0600

    Moved ImageWindow class to shared, added ImageWindow display

--- cnn_navigator/scripts/drone_simulator.py
+++ cnn_navigator/scripts/drone_simulator.py
@@ -15,17 +15,21 @@ from io import BytesIO
 import sys, os, pdb
 sys.path.append(os.path.abspath('../..'))  # Not clean
 from annotate.base import AnnotateBase
+from shared.imagewindow import ImageWindow
 
 ns = '/bebop/'
 
 
 
 class DroneSimulator(AnnotateBase):
-    def __init__(self):
+    def __init__(self, display=False):
         super(DroneSimulator, self).__init__()
         self.frame = 0
         self.msg = Image()
         self.camera  = rospy.Publisher(ns+'image_raw', Image, latch=True, queue_size=1)
+        self.display = display
+        self.iw = None
+
 
     # Consider re-using from Annotator
     def _load_bag_data(self, file):
@@ -39,11 +43,17 @@ class DroneSimulator(AnnotateBase):
 
         assert self.width==856 and self.height==480, "Unexpected image dimensions (%d, %d)" % (self.width, self.height)
 
+
     def next_image(self):
         i = self.frame % self.num_images
         image = PILImage.open(BytesIO(self.image_data[i]))
+
+        # This logic should probably reside elsewhere
+        if self.display:
+            self.iw.show_image(image).update()
         self.frame += 1
         return image.tobytes()
+
 
     def image_from_data(self, data):
         self.msg.height = self.height
@@ -58,6 +68,10 @@ class DroneSimulator(AnnotateBase):
 
     def simulate(self, file):
         self._load_bag_data(file)
+
+        # This logic belongs elsewhere
+        if self.display:
+            self.iw = ImageWindow(self.width, self.height)
 
         # Initialize
         rospy.init_node('drone_simulator')
