1a181dbff16b4fc80bc0ab4edc89346fb0566773
Jon Courtney <jon.courtney@gmail.com>
2017-06-17 21:59:21 -0600

    Fixed syntax error

--- annotate/play.py
+++ annotate/play.py
@@ -7,7 +7,7 @@ import numpy as np
 import argparse, sys, os, pdb
 
 sys.path.append(os.path.abspath('..'))
-from share.annotate_base import AnnotateBase
+from shared.annotate_base import AnnotateBase
 from shared.action import Action
 from shared.imagewindow import ImageWindow
 
