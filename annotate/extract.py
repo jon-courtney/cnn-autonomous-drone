32a3edd10206fee4c72c83372641b6ce82554f32
Jon Courtney <jon.courtney@gmail.com>
2017-06-19 02:42:11 -0600

    Added method to extract jpegs

--- annotate/extract.py
+++ annotate/extract.py
@@ -18,12 +18,20 @@ class Extractor(BagReader):
     def save(self, outfile):
         np.savez(outfile, image_data=self.image_data)
 
+    def write_jpegs(self, prefix='bag-', num=-1):
+        if num > self.num_images or num < 0:
+            num = self.num_images
+        for i in range(num):
+            with open(prefix+'-'+str(i)+'.jpg', 'wb') as f:
+                f.write(self.image_data[i])
+            print('{} / {}'.format(i, num))
 
 def get_args():
     parser = argparse.ArgumentParser(description='Extract drone flight images from ROS bag files. NOTE: Python 2 required.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('infile', metavar='<bagfile_in>', help='bagfile to analyze')
     parser.add_argument('outfile', metavar='<npzfile_out>', help='npz file for writing results')
     parser.add_argument('--oldtopic', default=False, action='store_true', help='if set, forces use of /bebop/image_raw_throttle/compressed topic')
+    parser.add_argument('--jpeg', default=False, action='store_true', help='write individual jpegs using <npzfile_out> as prefix')
 
     return parser.parse_args()
 
@@ -33,4 +41,8 @@ if __name__ == '__main__':
 
     e =  Extractor(newtopic=not args.oldtopic)
     e.extract(args.infile)
-    e.save(args.outfile)
+
+    if(args.jpeg):
+        e.write_jpegs(args.outfile)
+    else:
+        e.save(args.outfile)
