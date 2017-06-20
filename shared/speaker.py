cb869283e8b200b29c28b39a21575962aaee1f69
Jon Courtney <jon.courtney@gmail.com>
2017-06-19 20:50:32 -0600

    Changed os.system() call to subprocess.Popen() for efficiency

--- shared/speaker.py
+++ shared/speaker.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
-import os
+import subprocess
 
 class Speaker:
     def __init__(self):
         self.voice = 'en-us'
+        self.words = None
 
     def set_voice(self, voice):
         self.voice = voice
 
     def speak(self, words):
-        os.system("espeak -v " + self.voice + " --stdout '" + words + "' | paplay")
+        subprocess.Popen(["espeak", "-v", self.voice, words])
+
+    def speak_new(self, words):
+        if words != self.words:
+            self.words = words
+            self.speak(words)
 
 if __name__ == '__main__':
     s = Speaker()
